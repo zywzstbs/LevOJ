c6e1960ba7b9c7986db502992fabedffdb0674fd
zhangyunwen <1493988058@qq.com>
2025-03-11 08:54:58 +0800

    未完成
    未完成输入为0时的处理，以及OJRE

--- P1108/main.cpp
+++ P1108/main.cpp
@@ -20,5 +20,44 @@ int main()
         str[0] = '0';
         str[1] = '0';
     }
+
+    //将二进制数四位一个转换为十六进制
+    char str2[1000];
+    for (int i = 0; i < (len + pad) / 4; i++)
+    {
+        char c;
+        int m = (str[4 * i] - '0') * 8 + (str[4 * i + 1] - '0') * 4 + (str[4 * i + 2] - '0') * 2 + (str[4 * i + 3] - '0');
+        if (m<=9)
+        {
+            c = m + '0';
+        }else
+        {
+            c = m + 'A' - 10;
+        }
+        str2[i] = c;
+        while (m == 0 && i == 0)
+        {
+            printf("0");
+            return 0;
+        }
+    }
+
+    //去除前导0
+    for (int i = 0;i <strlen(str2);i++)
+    {
+        if(str2[i] == '0' && strlen(str2) > 1)
+        {
+            for (int j = 0;j <strlen(str2);j++)
+            {
+                str2[j] = str2[j + 1];
+            }
+        }
+    }
+
+    for (int i = 0;i < strlen(str2);i++)
+    {
+        printf("%c",str2[i]);
+    }
+
     return 0;
 }
